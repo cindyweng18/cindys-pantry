f426ac7248758c2113f175c599c11c4bd6111bc7
cindyweng18 <cindyweng18@gmail.com>
2024-07-30 15:18:40 -0400

    get items from firebase succesfully

--- app/page.js
+++ app/page.js
@@ -1,14 +1,30 @@
 import * as React from 'react';
+// import { useEffect } from 'react';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
+import {firebase, firestore} from "../firebase";
+import { getDocs, collection, getFirestore, query, onSnapshot} from 'firebase/firestore';
 
 const items = ['tomato', 'potato', 'onions', 'garlic']
-
+let itemsArr = [];
 export default function Home() {
+
+  const q = query(collection(firestore, 'pantry'))
+  const u = onSnapshot(q, (querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      itemsArr.push({...doc.data(), id: doc.id});
+    })
+  })
+
   return(
-    <Box sx={{ width: '100%' }}>
-      <Stack spacing={2} overflow={'auto'}>
+    <Box 
+    width="100vw"
+    height="100vh"
+    display={'flex'}
+    justifyContent={'center'}
+    alignItems={'center'}>
+      <Box width="800px" height="100px" display={'flex'} justifyContent={'center'} alignItems={'center'}> Cindy's Pantry </Box>
+      <Stack width="800px" height="300px" spacing={2} overflow={'auto'}>
         {items.map((i) => (
           <Box
             key={i}
